3efaef195bc18b36d4c9e8a7ced4b1592afb4d76
jiraphat <jiraphat42242@gmail.com>
2022-12-23 16:46:58 +0700

    finish

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -3,7 +3,21 @@
 #include<string>
 using namespace std;
 
-//[Missing Code 1] Write definition of the function findGrade() here.
+char x;
+char findGrade(double point){
+    if(point > 90)
+        x = 'A';
+    else if(point > 75)
+        x = 'B';
+    else if(point > 60)
+        x = 'C';
+    else if(point > 45)
+        x = 'D';
+    else 
+        x = 'F';
+    return x;
+    }
+    
 
 int main(){
 	//Input the number of students
@@ -18,10 +32,13 @@ int main(){
 		cout << "Name of student " << i+1 << ": ";
 		cin.ignore();
 		//[Missing Code 2] Get name of the i-th students that may include whitespace.
+		getline(cin,name[i]);
 		cout << "Score of student " << i+1 << ": ";
 		//[Missing Code 3] Get score of the i-th students.
+		cin >> score[i];
 		i++;
 	}
+
 	
 	//Print names scores and grades
 	i = 0;
--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,13 +1,45 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+int age ,height ,bounty;
+string x;
+    cout << "Enter your age: ";
+    cin >> age;
+    if(age <= 25){
+        cout << "Enter your height: ";
+        cin >> height;
+    if(height < 100){
+        x = "Chopper";
+    }
+    else 
+    if(height < 180){
+        x = "Usopp";
+    }
+    else{
+    cout << "Enter your bounty: ";
+    cin >> bounty;
+    
+    if(bounty > 1100000000){
+        x = "Zoro";
+    }
+    else{
+        x = "Sanji";
+    }
+    }
+    }
+    else if(age <= 60){
+        cout << "Enter your bounty: ";
+        cin >> bounty;
+        if(bounty > 500000000){
+            x = "Jinbe";
+        }
+        else
+            x = "Franky";
+    }
+    else
+        x = "Brook";
+cout << "Your character = " << x;
+return 0;
+}
